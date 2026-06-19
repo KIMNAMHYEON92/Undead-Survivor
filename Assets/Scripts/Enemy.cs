82023b3621ca05e5986b0223b942e25d78461b5e
KIMNAMHYEON <skagus912@gmail.com>
2026-06-19 12:16:01 +0900

    feat: 적 flipX로 스프라이트 방향전환

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -46,5 +46,10 @@ public class Enemy : MonoBehaviour
         // 4. 잔여 속도 제거 - 플레이어랑 충돌하면, 물리 속도를 0으로
         rigid.linearVelocity = Vector2.zero;
     }
+
+    void LateUpdate()
+    {
+        spriter.flipX= target.position.x < rigid.position.x;
+    }
     
 }
