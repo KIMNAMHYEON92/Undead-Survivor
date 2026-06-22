80d1643c6ddbf07d0301df0c22555ecb118d3ce1
KIMNAMHYEON <skagus912@gmail.com>
2026-06-22 11:12:54 +0900

    feat: GameManager 게임 시간 관리(난이도 관리용)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,25 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public Player player;
     public PoolManager pool;
+    // 흐르는 게임 시간 - 난이도 계산용도
+    public float gameTime;
+    // 최대 게임 시간 - 난이도 증가한도
+    public float maxGameTime = 10f;
 
     private void Awake()
     {
         instance = this; 
     }
+
+    void Update()
+    {
+        // 매 프레임마다 실제 흐른 시간을 누적
+        gameTime += Time.deltaTime;
+        // 
+        if (gameTime >= maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+    }
+    
 }
